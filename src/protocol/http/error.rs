161c9590ea1ef9afd57f5c9492c9667e1fc22128
xrelkd <46590321+xrelkd@users.noreply.github.com>
2020-07-09 12:38:49 +0800

    Enhance HTTP request parsing

--- src/protocol/http/error.rs
+++ src/protocol/http/error.rs
@@ -2,22 +2,33 @@ use snafu::Snafu;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("StdIo error: {}", source))]
-    StdIo { source: std::io::Error },
-
     #[snafu(display("Unsupported method: {}", method))]
     UnsupportedMethod { method: String },
 
-    #[snafu(display("Bad request"))]
-    BadRequest,
+    #[snafu(display("Could not parse HTTP request, error: {}", source))]
+    ParseRequest { source: httparse::Error },
 
-    #[snafu(display("Bad response"))]
-    BadResponse,
+    #[snafu(display("Could not parse HTTP response, error: {}", source))]
+    ParseResponse { source: httparse::Error },
 
     #[snafu(display("Host is unreachable"))]
     HostUnreachable,
-}
 
-impl From<std::io::Error> for Error {
-    fn from(source: std::io::Error) -> Error { Error::StdIo { source } }
+    #[snafu(display("No HTTP method provided"))]
+    NoMethodProvided,
+
+    #[snafu(display("No HTTP path provided"))]
+    NoPathProvided,
+
+    #[snafu(display("Invalid HTTP method: {}", method))]
+    InvalidMethod { method: String },
+
+    #[snafu(display("Invalid path: {}", path))]
+    InvalidPath { path: String },
+
+    #[snafu(display("Invalid HTTP header name: {}", name))]
+    InvalidHeaderName { name: String },
+
+    #[snafu(display("Invalid HTTP header value: {}", value))]
+    InvalidHeaderValue { value: String },
 }
--- src/service/http/error.rs
+++ src/service/http/error.rs
@@ -4,18 +4,23 @@ use crate::{protocol, transport};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("StdIo error: {}", source))]
-    StdIo { source: std::io::Error },
+    #[snafu(display("Could not read buffer, error: {}", source))]
+    ReadBuf { source: std::io::Error },
+
+    #[snafu(display("Could not write stream error: {}", source))]
+    WriteStream { source: std::io::Error },
+
+    #[snafu(display("Error occurred while shutting down TCP stream, error: {}", source))]
+    Shutdown { source: std::io::Error },
+
+    #[snafu(display("HTTP request is too large"))]
+    RequestTooLarge,
 
     #[snafu(display("Transport error: {}", source))]
     Transport { source: transport::Error },
 
     #[snafu(display("Protocol error: {}", source))]
     Protocol { source: protocol::http::Error },
-}
-
-impl From<std::io::Error> for Error {
-    fn from(source: std::io::Error) -> Error { Error::StdIo { source } }
 }
 
 impl From<transport::Error> for Error {
