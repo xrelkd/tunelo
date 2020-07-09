0edff81a1e6a20462596c2bd29ac6a44e0f77a63
xrelkd <46590321+xrelkd@users.noreply.github.com>
2020-07-09 19:18:40 +0800

    Enhance HTTP tunnel handshaking with httparse

--- src/client/handshake/error.rs
+++ src/client/handshake/error.rs
@@ -19,6 +19,9 @@ pub enum Error {
     #[snafu(display("Could not parse SOCKS5, error: {}", source))]
     ParseSocks5Reply { source: SocksError },
 
+    #[snafu(display("Could not parse HTTP repsonse, error: {}", source))]
+    ParseHttpResponse { source: httparse::Error },
+
     #[snafu(display("Host is unreachable"))]
     HostUnreachable,
 
@@ -38,6 +41,9 @@ pub enum Error {
     #[snafu(display("Invalid SOCKS4a id: {}", String::from_utf8_lossy(id)))]
     InvalidSocks4aId { id: Vec<u8> },
 
+    #[snafu(display("No HTTP response code"))]
+    NoHttpResponseCode,
+
     #[snafu(display("Unsupported HTTP method: {}", method))]
     UnsupportedHttpMethod { method: String },
 
@@ -46,4 +52,10 @@ pub enum Error {
 
     #[snafu(display("Bad HTTP response"))]
     BadHttpResponse,
+
+    #[snafu(display("HTTP response is too large"))]
+    HttpResponseTooLarge,
+
+    #[snafu(display("Could not build HTTP request, error: {}", source))]
+    BuildHttpRequest { source: std::fmt::Error },
 }
--- src/client/handshake/http.rs
+++ src/client/handshake/http.rs
@@ -1,9 +1,14 @@
-use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use bytes::BytesMut;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::{
     client::handshake::{ClientHandshake, Error},
     common::HostAddress,
 };
+
+const INITIAL_BUF_SIZE: usize = 128;
+const BUF_ADDITIONAL_SIZE: usize = 128;
+const MAX_BUF_SIZE: usize = 512;
 
 impl<Stream> ClientHandshake<Stream>
 where
@@ -15,57 +20,71 @@ where
         user_agent: Option<&str>,
     ) -> Result<(), Error>
     where
-        Stream: AsyncRead + AsyncReadExt + AsyncWrite + AsyncWriteExt + Unpin,
+        Stream: AsyncRead + AsyncWrite + Unpin,
     {
         let request = {
+            use std::fmt::Write;
             let host = target_host.to_string();
-
-            let mut req = format!("CONNECT {} HTTP/1.1\r\n", host);
-            req.push_str(&format!("Host: {}\r\n", host));
+            let mut req = BytesMut::with_capacity(128);
+            write!(req, "CONNECT {} HTTP/1.1\r\n", host)
+                .map_err(|source| Error::BuildHttpRequest { source })?;
+            write!(req, "Host: {}\r\n", host)
+                .map_err(|source| Error::BuildHttpRequest { source })?;
 
             if let Some(ua) = user_agent {
-                req.push_str(&format!("User-Agent: {}\r\n", ua));
+                write!(req, "User-Agent: {}\r\n", ua)
+                    .map_err(|source| Error::BuildHttpRequest { source })?;
             }
 
-            req.push_str("\r\n");
+            write!(req, "\r\n").map_err(|source| Error::BuildHttpRequest { source })?;
             req
         };
-        self.stream
-            .write(request.as_bytes())
-            .await
-            .map_err(|source| Error::WriteStream { source })?;
+        self.stream.write(request.as_ref()).await.map_err(|source| Error::WriteStream { source })?;
 
-        let mut lines_reader = tokio::io::BufReader::new(&mut self.stream).lines();
-        match lines_reader.next_line().await.map_err(|source| Error::ReadStream { source })? {
-            None => return Err(Error::BadHttpResponse),
-            Some(line) => {
-                let mut parts = line.split_whitespace();
-
-                // HTTP version
-                if let None = parts.next() {
-                    return Err(Error::BadHttpResponse);
+        let mut buf = BytesMut::with_capacity(INITIAL_BUF_SIZE);
+        let msg = loop {
+            let _n = self
+                .stream
+                .read_buf(&mut buf)
+                .await
+                .map_err(|source| Error::ReadStream { source })?;
+            match parse_header(&mut buf)? {
+                None => {
+                    if buf.capacity() < MAX_BUF_SIZE {
+                        buf.reserve(std::cmp::min(
+                            BUF_ADDITIONAL_SIZE,
+                            MAX_BUF_SIZE - buf.capacity(),
+                        ));
+                        continue;
+                    }
+                    return Err(Error::HttpResponseTooLarge);
                 }
-
-                // status code
-                match parts.next() {
-                    Some(status_code) => match status_code {
-                        "200" => {}
-                        "401" | "402" | "403" | "404" => return Err(Error::HostUnreachable),
-                        _ => return Err(Error::HostUnreachable),
-                    },
-                    None => return Err(Error::BadHttpResponse),
-                }
+                Some(msg) => break msg,
             }
-        }
+        };
 
-        while let Some(line) =
-            lines_reader.next_line().await.map_err(|source| Error::ReadStream { source })?
-        {
-            if line.is_empty() {
-                break;
-            }
+        match msg.status_code {
+            200 => Ok(()),
+            401 | 402 | 403 | 404 => Err(Error::HostUnreachable),
+            _ => Err(Error::HostUnreachable),
         }
+    }
+}
 
-        Ok(())
+struct ParsedMessage {
+    status_code: u16,
+}
+
+fn parse_header(buf: &mut BytesMut) -> Result<Option<ParsedMessage>, Error> {
+    let mut headers = [httparse::EMPTY_HEADER; 32];
+    let mut response = httparse::Response::new(&mut headers);
+    match response.parse(&buf[..]) {
+        Err(source) => Err(Error::ParseHttpResponse { source }),
+        Ok(httparse::Status::Partial) => Ok(None),
+        Ok(httparse::Status::Complete(parsed_len)) => {
+            let status_code = response.code.ok_or(Error::NoHttpResponseCode)?;
+            let _header_buf = buf.split_to(parsed_len).freeze();
+            Ok(Some(ParsedMessage { status_code }))
+        }
     }
 }
