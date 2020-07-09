fc7ff845be5eb198508a0dae6c0efd0cd17fd65c
xrelkd <46590321+xrelkd@users.noreply.github.com>
2020-07-09 12:38:49 +0800

    feat: enhance HTTP request parsing

--- src/service/http/service.rs
+++ src/service/http/service.rs
@@ -1,5 +1,7 @@
-use std::str::FromStr;
-use std::{net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
+
+use bytes::BytesMut;
+use http::{header::HeaderName, HeaderMap, HeaderValue, Method};
 
 use tokio::{
     io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
@@ -13,6 +15,25 @@ use crate::{
     service::http::Error,
     transport::Transport,
 };
+
+const INITIAL_BUF_SIZE: usize = 256;
+const BUF_ADDITIONAL_SIZE: usize = 128;
+const MAX_HEADER_BUF_SIZE: usize = 512;
+
+struct ParsedMessage {
+    req_method: Method,
+    headers: HeaderMap,
+    path: String,
+}
+
+impl ParsedMessage {
+    fn host_address(&self) -> Option<HostAddress> {
+        match self.headers.get(http::header::HOST) {
+            Some(host) => HostAddress::from_str(host.to_str().unwrap_or_default()).ok(),
+            None => HostAddress::from_str(&self.path).ok(),
+        }
+    }
+}
 
 pub struct Service<TransportStream> {
     transport: Arc<Transport<TransportStream>>,
@@ -30,43 +51,123 @@ where
         Service { transport, _authentication_manager: authentication_manager }
     }
 
+    fn parse_header<'buf>(buf: &'buf mut BytesMut) -> Result<Option<ParsedMessage>, ProtocolError> {
+        if buf.is_empty() {
+            return Ok(None);
+        }
+
+        let mut empty_headers = [httparse::EMPTY_HEADER; 32];
+        let mut request = httparse::Request::new(&mut empty_headers);
+        let status = request
+            .parse(&buf.as_ref())
+            .map_err(|source| ProtocolError::ParseRequest { source })?;
+
+        match status {
+            httparse::Status::Partial => return Ok(None),
+            httparse::Status::Complete(parsed_len) => {
+                let method = request.method.ok_or(ProtocolError::NoMethodProvided)?;
+                let method = Method::from_bytes(method.as_bytes())
+                    .map_err(|_| ProtocolError::InvalidMethod { method: method.to_owned() })?;
+
+                let path =
+                    request.path.map(|p| p.to_string()).ok_or(ProtocolError::NoPathProvided)?;
+
+                let mut headers = HeaderMap::with_capacity(request.headers.len());
+                for header in request.headers {
+                    let name = HeaderName::from_str(header.name).map_err(|_| {
+                        ProtocolError::InvalidHeaderName { name: header.name.to_string() }
+                    })?;
+                    let value = HeaderValue::from_bytes(header.value).map_err(|_| {
+                        ProtocolError::InvalidHeaderValue {
+                            value: String::from_utf8_lossy(header.value).to_string(),
+                        }
+                    })?;
+                    headers.append(name, value);
+                }
+
+                let _header_buf = buf.split_to(parsed_len);
+                Ok(Some(ParsedMessage { req_method: method, headers, path }))
+            }
+        }
+    }
+
     pub async fn handle(
         &self,
         mut client_stream: TransportStream,
         _client_addr: SocketAddr,
     ) -> Result<(), Error> {
-        let remote_host = match Self::parse_request(&mut client_stream).await {
-            Ok(r) => r,
-            Err(ProtocolError::BadRequest) => {
-                Self::shutdown_with_status(&mut client_stream, StatusCode::BadRequest).await?;
-                return Ok(());
+        let mut buf = BytesMut::with_capacity(INITIAL_BUF_SIZE);
+        let msg = loop {
+            let _n = client_stream
+                .read_buf(&mut buf)
+                .await
+                .map_err(|source| Error::ReadBuf { source })?;
+            match Self::parse_header(&mut buf) {
+                Ok(Some(msg)) => break msg,
+                Ok(None) => {
+                    if buf.capacity() < MAX_HEADER_BUF_SIZE {
+                        buf.reserve(std::cmp::min(
+                            BUF_ADDITIONAL_SIZE,
+                            MAX_HEADER_BUF_SIZE - buf.capacity(),
+                        ));
+                        continue;
+                    }
+                    return Err(Error::RequestTooLarge);
+                }
+                Err(err) => match err {
+                    ProtocolError::ParseRequest { .. } => {
+                        Self::shutdown_with_status(&mut client_stream, StatusCode::BadRequest)
+                            .await?;
+                        return Ok(());
+                    }
+                    ProtocolError::HostUnreachable => {
+                        Self::shutdown_with_status(&mut client_stream, StatusCode::NotFound)
+                            .await?;
+                        return Ok(());
+                    }
+                    err => return Err(err)?,
+                },
             }
-            Err(ProtocolError::HostUnreachable) => {
-                Self::shutdown_with_status(&mut client_stream, StatusCode::NotFound).await?;
-                return Ok(());
-            }
-            Err(ProtocolError::UnsupportedMethod { method }) => {
-                client_stream.write(StatusCode::NotImplemented.status_line().as_bytes()).await?;
-                client_stream
-                    .write(
-                        format!(
-                            "<html lang=\"en\"><body>HTTP method {} is NOT \
-                             SUPPORTED</body></html>\r\n",
-                            method
-                        )
-                        .as_bytes(),
+        };
+
+        if msg.req_method != Method::CONNECT {
+            client_stream
+                .write(StatusCode::NotImplemented.status_line().as_bytes())
+                .await
+                .map_err(|source| Error::WriteStream { source })?;
+            client_stream
+                .write(
+                    format!(
+                        "<html lang=\"en\"><body>HTTP method {} is NOT SUPPORTED</body></html>\r\n",
+                        msg.req_method
                     )
-                    .await?;
-                client_stream.shutdown().await?;
+                    .as_bytes(),
+                )
+                .await
+                .map_err(|source| Error::WriteStream { source })?;
+            client_stream.shutdown().await.map_err(|source| Error::Shutdown { source })?;
+            return Ok(());
+        }
+
+        let remote_host = match msg.host_address() {
+            Some(r) => r,
+            None => {
+                client_stream
+                    .write(StatusCode::NotFound.status_line().as_bytes())
+                    .await
+                    .map_err(|source| Error::WriteStream { source })?;
+                client_stream.shutdown().await.map_err(|source| Error::Shutdown { source })?;
                 return Ok(());
             }
-            Err(err) => return Err(err.into()),
         };
 
         let (remote_socket, _remote_addr) = match self.transport.connect(&remote_host).await {
             Ok((socket, addr)) => {
                 let response = "HTTP/1.1 200 Connection Established\r\n\r\n";
-                let _n = client_stream.write(response.as_ref()).await?;
+                let _n = client_stream
+                    .write(response.as_ref())
+                    .await
+                    .map_err(|source| Error::WriteStream { source })?;
                 (socket, addr)
             }
             Err(_err) => return Err(Error::Protocol { source: ProtocolError::HostUnreachable }),
@@ -84,87 +185,14 @@ where
         stream: &mut TransportStream,
         status_code: StatusCode,
     ) -> Result<(), Error> {
-        stream.write(status_code.status_line().as_bytes()).await?;
-        stream.shutdown().await?;
+        stream
+            .write(status_code.status_line().as_bytes())
+            .await
+            .map_err(|source| Error::WriteStream { source })?;
+        stream.shutdown().await.map_err(|source| Error::Shutdown { source })?;
         Ok(())
-    }
-
-    async fn parse_request(stream: &mut TransportStream) -> Result<HostAddress, ProtocolError> {
-        use bytes::BytesMut;
-        let mut buf = BytesMut::with_capacity(384);
-        let _n = stream.read_buf(&mut buf).await?;
-
-        let mut headers = [httparse::EMPTY_HEADER; 16];
-        let mut request = httparse::Request::new(&mut headers);
-        let status = request.parse(&buf.as_ref()).map_err(|_source| ProtocolError::BadRequest)?;
-
-        let remote_host = {
-            match status {
-                httparse::Status::Partial => return Err(ProtocolError::BadRequest),
-                httparse::Status::Complete(_parsed_len) => {
-                    let method = request.method.ok_or(ProtocolError::BadRequest)?;
-                    if method.to_uppercase() != "CONNECT" {
-                        return Err(ProtocolError::UnsupportedMethod { method: method.to_owned() });
-                    }
-
-                    let mut remote_host =
-                        request.path.map(|p| p.to_string()).ok_or(ProtocolError::BadRequest)?;
-
-                    for header in request.headers {
-                        if header.name.to_lowercase() == "host" {
-                            remote_host = String::from_utf8_lossy(header.value).to_string();
-                            break;
-                        }
-                    }
-
-                    remote_host
-                }
-            }
-        };
-
-        let remote_host =
-            HostAddress::from_str(&remote_host).map_err(|_| ProtocolError::BadRequest)?;
-
-        Ok(remote_host)
     }
 }
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let raw_request = r#"CONNECT cdnjs.cloudflare.com:443 HTTP/1.1
-User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0
-Proxy-Connection: keep-alive
-Connection: keep-alive
-Host: cdnjs.cloudflare.com:443"#;
-
-        let raw_request2 = r#"CONNECT www.google.com:443 HTTP/1.1
-User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0
-Proxy-Connection: keep-alive
-Connection: keep-alive
-Host: www.google.com:443"#;
-
-        let raw_request3 = r#"GET http://httpbin.org/ HTTP/1.1
-Host: httpbin.org
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-Accept-Language: zh-TW,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3
-Accept-Encoding: gzip, deflate
-DNT: 1
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1
-"#;
-
-        let raw_request4 = r#"GET http://myip.com.tw/ HTTP/1.1
-Host: myip.com.tw
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-Accept-Language: zh-TW,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3
-Accept-Encoding: gzip, deflate
-DNT: 1
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1
-"#;
-    }
-}
+mod tests {}
