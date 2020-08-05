9fdb000070292254fce8f3725b4045b851f694a5
xrelkd <46590321+xrelkd@users.noreply.github.com>
2020-08-05 12:51:01 +0800

    chore: update `shell.nix`
      - Apply `niv`

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
-with import <nixpkgs> { };
-stdenv.mkDerivation {
-  name = "tunelo-dev";
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  inherit (pkgs) stdenv;
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+
+    git
+    rustup
+    cargo-make
+  ];
 
   RUST_BACKTRACE = 1;
-
-  nativeBuildInputs = [ rustup ];
 }
